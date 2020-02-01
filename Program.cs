c7d11e086db7bd3dcf8739e9a72bd8d4979089d1
Zach Jones <zacharytjones1@gmail.com>
2020-02-01 13:45:21 -0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MakePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 255; i++){
+                Console.WriteLine(i);
+            }
+            for (int i = 0; i <=100; i++){
+                if(i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine("Can't do both");
+                } else if(i % 3 == 0){
+                    Console.WriteLine(i);
+                } else if(i % 5 == 0){
+                    Console.WriteLine(i);
+                }
+            }
+            for (int i = 0; i <=100; i++){
+                if(i % 3 == 0 && i % 5 == 0){
+                    Console.WriteLine("FizzBuzz");
+                } else if(i % 3 == 0){
+                    Console.WriteLine("Fizz");
+                } else if(i % 5 == 0){
+                    Console.WriteLine("Buzz"
+                    );
+                }
+            }
+        }
+    }
+}
