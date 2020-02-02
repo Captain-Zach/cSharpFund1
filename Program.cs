f42576137c3ad1505c2519bcd240b720b2a55847
Zach Jones <zacharytjones1@gmail.com>
2020-02-01 17:57:04 -0800

    New things added for more class

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MakePractice
 {
@@ -24,9 +25,45 @@ namespace MakePractice
                 } else if(i % 3 == 0){
                     Console.WriteLine("Fizz");
                 } else if(i % 5 == 0){
-                    Console.WriteLine("Buzz"
-                    );
+                    Console.WriteLine("Buzz");
                 }
+            }
+            coll_practice();
+
+        }
+        static void coll_practice(){
+            // arrays
+            int[] oneThrough = new int[] {0,1,2,3,4,5,6,7,8,9};
+            string[] names = new string[] {"Tim", "Martin", "Nikki", "Sara"};
+            bool[] flips = new bool[10];
+            for(int i = 0; i < flips.Length; i++){
+                if(i % 2 == 0){
+                    flips[i] = true;
+                }else{
+                    flips[i] = false;
+                }
+            }
+            List<string> iceCream = new List<string>();
+            iceCream.Add("Dookie");
+            iceCream.Add("Chocolate");
+            iceCream.Add("Vanilla");
+            iceCream.Add("Strawberry");
+            iceCream.Add("Gun");
+            Console.WriteLine(iceCream.Count);
+            Console.WriteLine(iceCream[2]);
+            iceCream.Remove(iceCream[2]);
+            Console.WriteLine(iceCream.Count);
+
+            Dictionary<string,string> strange = new Dictionary<string,string>();
+            for(int i = 0; i < names.Length; i++){
+                strange.Add(names[i], iceCream[i]);
+                Console.WriteLine(i);
+
+            }
+            Console.WriteLine(strange);
+            foreach(var entry in strange){
+                Console.WriteLine(entry.Key);
+                Console.WriteLine(entry.Value);
             }
         }
     }
